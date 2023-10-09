e722912901f6771f115013f9793aebc14bc5ff21
linhbh02 <linhbhce160280@fpt.edu.vn>
2023-10-10 05:43:32 +0700

    init project

---
+++ Assets/Script/AboutGame.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class AboutGame : MonoBehaviour
+{
+    
+    public void BackButton()
+    {
+        SceneManager.LoadScene(1); // Load scene menu khi nhấn vào button
+    }
+}
